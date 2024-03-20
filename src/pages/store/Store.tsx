1cefe5b714e0ca033ef36d923467f57ecea63443
jesusre890 <jesusre890@gmail.com>
2024-03-20 11:13:32 -0300

    add styles in store components

--- src/pages/store/Store.tsx
+++ src/pages/store/Store.tsx
@@ -3,15 +3,22 @@ import { StoreProps } from "./store.types";
 import { PageTitle } from "@/components/PageTitle";
 import { CollectionContainer } from "./components/CollectionContainer";
 import { Spinner } from "@/components/Spinner";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
+import AOS from "aos";
 
 export const Store: React.FC<StoreProps> = ({
 	response,
 	isLoading,
 	isStoreOpen,
 }) => {
+		useEffect(() => {
+			AOS.init({ duration: 2000 });
+		});
+
 	if (!isStoreOpen) {
 		return (
-			<div>
+			<div className="flex flex-col text-center">
 				<PageTitle title="Tienda" id="productos" />
 				<h1>La tienda se encuentra cerrada</h1>
 				<p>Los pedidos en la tienda se pueden hacer:</p>
@@ -21,9 +28,8 @@ export const Store: React.FC<StoreProps> = ({
 		);
 	}
 	if (isLoading) return <Spinner />;
-
 	return (
-		<div>
+		<div data-aos="fade-up" className="flex flex-col">
 			<PageTitle title="Tienda" id="productos" />
 			{response?.data && <CollectionContainer collections={response.data} />}
 		</div>
